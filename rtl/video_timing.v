2d40bfd54a304631e4d17b8f51d5a88cbcc6cddf
atrac17 <32810066+atrac17@users.noreply.github.com>
2023-05-15 16:20:37 -0700

    Corrected Video Timings
    Not reported accurately in MAME, taken from PCB. This should resolve #1.

--- rtl/video_timing.v
+++ rtl/video_timing.v
@@ -26,17 +26,17 @@ module video_timing
 
 wire [8:0] h_ofs = 0;
 wire [8:0] HBL_START  = 255;
-wire [8:0] HBL_END    = refresh_mod ? 383 : 405;
+wire [8:0] HBL_END    = 383;
 wire [8:0] HS_START   = HBL_START + 41 + $signed(hs_offset);
-wire [8:0] HS_END     = HBL_START + ( refresh_mod ? 66 : 76 ) + $signed(hs_offset) + $signed(hs_width);
-wire [8:0] HTOTAL     = 405;
+wire [8:0] HS_END     = HBL_START + 66 + $signed(hs_offset) + $signed(hs_width);
+wire [8:0] HTOTAL     = 383;
 
 wire [8:0] v_ofs = 0;
 wire [8:0] VBL_START  = 240;
 wire [8:0] VBL_END    = 16;
 wire [8:0] VS_START   = VBL_START + 13 + $signed(vs_offset);
 wire [8:0] VS_END     = VBL_START + 21 + $signed(vs_offset) + $signed(vs_width);
-wire [8:0] VTOTAL     = 262;
+wire [8:0] VTOTAL     = ( refresh_mod ? 262 : 274 );
 
 
 reg [8:0] v;
@@ -55,9 +55,9 @@ always @ (posedge clk) begin
 
         hsync <= 0;
         vsync <= 0;
-    end else if ( clk_pix == 1 ) begin 
+    end else if ( clk_pix == 1 ) begin
         // counter
-        if (h == HTOTAL - (refresh_mod ? 22 : 0)) begin
+        if ( h == HTOTAL ) begin
             h <= 0;
             v <= v + 1'd1;
 
@@ -81,7 +81,7 @@ always @ (posedge clk) begin
         end else if ( v == VBL_END ) begin
             vbl <= 0;
         end
-   
+
         if ( v == (VS_START ) ) begin
             vsync <= 1;
         end else if ( v == (VS_END ) ) begin
